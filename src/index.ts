9f58eb6169d18acb77137ce118e0f82934f3764b
HussainYousuf <hussainroker@gmail.com>
2023-05-19 09:10:46 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+type PositiveTemporal =
+  | { days: string }
+  | { weeks: string }
+  | { months: string }
+  | { years: string }
+  | { hours: string }
+  | { minutes: string };
+
+export const toPositiveTemporal = (amount, unit, nonNegative) => {
+  // your code here
+  return {};
+};
